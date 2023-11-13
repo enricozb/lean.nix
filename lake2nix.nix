586de5fa173ba754bbe699618010ac9187355c01
Enrico Zandomeni Borba <enricozb@gmail.com>
2023-11-13 11:44:32 -0500

    let binding to make recursive function

--- lake2nix.nix
+++ lake2nix.nix
@@ -13,64 +13,63 @@ let
       rev = dep.rev;
       ref = dep."inputRev?";
     };
-in {
-# the name of the lake package being built
-name,
 
-# the package source
-src,
+  lake2nix = {
+    # the name of the lake package being built
+    name,
+    # the package source
+    src,
+    # the nix system being built for
+    system ? builtins.currentSystem,
+    # an object containing the `buildLeanPackage` function
+    lean-toolchain ? null,
+    # a path to the `lake-manifest.json` file
+    lake-manifest-file ? "${src}/lake-manifest.json",
+    # path to the `lean-toolchain` file
+    lean-toolchain-file ? "${src}/lean-toolchain" }:
 
-# the nix system being built for
-system ? builtins.currentSystem,
-
-# an object containing the `buildLeanPackage` function
-lean-toolchain ? null,
-
-# a path to the `lake-manifest.json` file
-lake-manifest-file ? "${src}/lake-manifest.json",
-
-# path to the `lean-toolchain` file
-lean-toolchain-file ? "${src}/lean-toolchain" }:
-
-let
-  lean = if !(builtins.isNull lean-toolchain) then
-    lean-toolchain
-  else
     let
-      # "leanprover/lean4:v4.2.0-rc1"
-      string = stripNewline (builtins.readFile lean-toolchain-file);
-      # [ "leanprover/lean4" "v4.2.0-rc1" ]
-      parts = builtins.filter builtins.isString (builtins.split ":" string);
-      # "leanprover/lean4"
-      repo = builtins.elemAt parts 0;
-      # "v4.2.0-rc1"
-      ref = builtins.elemAt parts 1;
-    in (import (builtins.fetchGit {
-      # TODO: should github be hardcoded?
-      url = "git@github.com:${repo}";
-      ref = "refs/tags/${ref}";
-    })).packages.${builtins.currentSystem};
+      lean = if !(builtins.isNull lean-toolchain) then
+        lean-toolchain
+      else
+        let
+          # "leanprover/lean4:v4.2.0-rc1"
+          string = stripNewline (builtins.readFile lean-toolchain-file);
+          # [ "leanprover/lean4" "v4.2.0-rc1" ]
+          parts = builtins.filter builtins.isString (builtins.split ":" string);
+          # "leanprover/lean4"
+          repo = builtins.elemAt parts 0;
+          # "v4.2.0-rc1"
+          ref = builtins.elemAt parts 1;
+        in (import (builtins.fetchGit {
+          # TODO: should github be hardcoded?
+          url = "git@github.com:${repo}";
+          ref = "refs/tags/${ref}";
+        })).packages.${builtins.currentSystem};
 
-  # get deps from lake-manifest-file if it exists
-  deps = if builtins.pathExists lake-manifest-file then
-    let
-      lake-manifest = builtins.fromJSON (builtins.readFile lake-manifest-file);
+      # get deps from lake-manifest-file if it exists
+      deps = if builtins.pathExists lake-manifest-file then
+        let
+          lake-manifest =
+            builtins.fromJSON (builtins.readFile lake-manifest-file);
 
-    in builtins.map (
+        in builtins.map (
 
-      { git }:
-      (lake2nix {
-        name = git.name;
-        src = fetchDep git;
-        system = system;
-        lean-toolchain = lean;
-      }).package
+          { git }:
+          (lake2nix {
+            name = git.name;
+            src = fetchDep git;
+            system = system;
+            lean-toolchain = lean;
+          }).package
 
-    ) lake-manifest.packages
-  else
-    [ ];
+        ) lake-manifest.packages
+      else
+        [ ];
 
-in {
-  lean = lean-toolchain;
-  package = lean.buildLeanPackage { inherit name src deps; };
-}
+    in {
+      lean = lean-toolchain;
+      package = lean.buildLeanPackage { inherit name src deps; };
+    };
+
+in lake2nix
